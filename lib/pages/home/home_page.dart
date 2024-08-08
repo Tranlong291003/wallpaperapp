3ab156d2f010b873e3431157a381e6fe5edac41b
Tranlong291003 <tranlong291003@gmail.com>
2024-08-08 13:49:16 +0700

    đủ code

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,6 +4,10 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text('home'),
+      ),
+    );
   }
 }
