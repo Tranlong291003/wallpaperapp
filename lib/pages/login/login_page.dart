6b9d53087c668c86970b63fd7c7a04e08b9616b3
Tranlong291003 <tranlong291003@gmail.com>
2024-08-08 13:40:06 +0700

    test

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -40,58 +40,7 @@ class _LoginScreenState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  hintText: 'Email',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-                onSaved: (value) => _email = value!,
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: 'Password',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
-                onSaved: (value) => _password = value!,
-              ),
-              const SizedBox(height: 32.0),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    // Implement login logic here
-                    print('Email: $_email, Password: $_password');
-                  }
-                },
-                child: const Text('Sign In'),
-              ),
-            ],
-          ),
-        ),
-      ),
+      appBar: AppBar(),
     );
   }
 }
--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:wallpaper/apps/theme/theme.dart';
+import 'package:wallpaper/apps/config/theme.dart';
 
 class ThemeProvider with ChangeNotifier {
   ThemeData _themeData;
